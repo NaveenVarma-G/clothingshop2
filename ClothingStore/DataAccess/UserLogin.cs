cc97bb44573abc5d6f779bde264914434e9d6c79
NeehaV <94872643+NeehaV@users.noreply.github.com>
2022-08-14 18:56:44 -0400

    validations added

--- ClothingStore/DataAccess/UserLogin.cs
+++ ClothingStore/DataAccess/UserLogin.cs
@@ -1,5 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel;
+using Microsoft.AspNetCore.Mvc;
 
 namespace ClothingStore.DataAccess
 {
@@ -7,7 +11,13 @@ namespace ClothingStore.DataAccess
     {
         public int CredentialId { get; set; }
         public int UserId { get; set; }
+
+        [Required(ErrorMessage ="Please enter a username")]
+        [MinLength(3, ErrorMessage = "Username length should be more than 3 characters.")]
         public string UserName { get; set; } = null!;
+
+        [Required(ErrorMessage = "Please enter a password")]
+        [RegularExpression(@"^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[^\da-zA-Z]).{8,15}$", ErrorMessage = "Password should atleast 8 characters long with one Capital letter,small letter, number and a special character")]
         public string Password { get; set; } = null!;
 
         public virtual UserDetail User { get; set; } = null!;
